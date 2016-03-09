27284c6d60f03014884e27800812aa437654f06d
sts1992 <teatea1992@gmail.com>
2016-03-09 22:33:06 +0900

    Added files via upload

---
+++ serch-ec2-sns.py
@@ -0,0 +1,42 @@
+
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    client = boto3.client('ec2')
+    sns = boto3.client('sns')
+
+    print('Loading function')
+    response = client.describe_instances( Filters=[
+            {
+                'Name': 'instance-state-name',
+                'Values': [
+                    'stopped'
+                ]
+            }
+        ]
+    )
+    print(response)
+
+    instance_ids = []
+    instance_type = []
+    info = []
+
+    i = len(response['Reservations'])
+    for a in range(0, i):
+        instance_ids.insert(a, response['Reservations'][a]['Instances'][0]['InstanceId'])
+        instance_type.insert(a, response['Reservations'][a]['Instances'][0]['InstanceType'])
+        info.insert(a, {instance_ids[a]:instance_type[a]})
+
+
+    topic = 'arn:aws:sns:ap-northeast-1:xxxxx'
+    subject = 'Your Email Subject!!'
+
+    response = sns.publish(
+        TopicArn=topic,
+        Message=str(info),
+        Subject=subject,
+    )
+
+    return info
